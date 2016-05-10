42d6a9d6daa74330ee96c773f4cffb59cb2d80d9
Chris Harris <chris.harris@theopenskyproject.com>
2016-05-10 13:05:54 -0700

    Renamed files

--- default.ps1
+++ default.ps1
@@ -1,6 +1,6 @@
 Task Default -Depends Test
 Task Test {
-    $testResults = Invoke-Pester -Path $PSScriptRoot -PassThru
+    $testResults = Invoke-Pester -Path "$PSScriptRoot\default.tests.ps1" -PassThru
     if ($testResults.FailedCount -gt 0) {
         $testResults | Format-List
         Write-Error -Message 'One or more Pester tests failed. Build cannot continue!'
